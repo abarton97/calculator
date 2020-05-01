f123749eac2eca75fbbd9833c263c5d220218bc0
abarton97 <austinbarton1997@gmail.com>
2020-05-01 10:42:08 -0600

    color update

--- script.js
+++ script.js
@@ -88,7 +88,22 @@ function getNumbers(e) {
     if (storedValue.length <= 8) {
       storedValue += e.target.innerText;
       display.innerText = storedValue;
+      color = true;
+      console.log(displayColor)
     }
+  }
+}
+
+let displayColor = "";
+let color;
+
+function changeColors(e) {
+  displayColor = e.target.innerText;
+  if (operatorPressed && !color) {
+    this.style.backgroundColor = "red";
+    console.log(displayColor)
+  } else if (color) {
+    //displayColor.style.backgroundColor = "white";
   }
 }
 
@@ -100,3 +115,6 @@ operators.forEach((operator) => operator.addEventListener("click", getOperators)
 
 const clear = document.querySelectorAll(".clear");
 clear.forEach((clear) => clear.addEventListener("click", clearDisplay));
+
+const colorops = document.querySelectorAll(".operator");
+colorops.forEach((colorop) => colorop.addEventListener("click", changeColors));
